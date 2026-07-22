7adfc21f008d3571bab1c95a2654edac68ee86b3
paulptrk <paul7098@hotmail.com>
2026-07-22 15:51:47 -0400

    Adjust "+ New Column" panel top offset

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -12,7 +12,7 @@ export default function Board() {
           index={index}
         />
       ))}
-      <button className="text-medium-grey hover:text-main-purple mt-9 flex h-[calc(100%-36px)] min-h-[200px] w-[280px] shrink-0 cursor-pointer items-center justify-center rounded-[6px] bg-gradient-to-b from-[rgba(43,44,55,0.25)] to-[rgba(43,44,55,0.13)] text-2xl font-bold">
+      <button className="text-medium-grey hover:text-main-purple mt-10 flex h-[calc(100%-40px)] min-h-[200px] w-[280px] shrink-0 cursor-pointer items-center justify-center rounded-[6px] bg-gradient-to-b from-[rgba(43,44,55,0.25)] to-[rgba(43,44,55,0.13)] text-2xl font-bold">
         + New Column
       </button>
     </div>
